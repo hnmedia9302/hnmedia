3ced99ec274f51f52a761fa01c78204bfbd60da9
hnmedia9302 <hnmedia555@gmail.com>
2024-12-25 21:26:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+
+// Example: Alert on form submission
+document.querySelector("form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Thank you for contacting HN Media! We'll get back to you shortly.");
+});
